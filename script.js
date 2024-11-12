e1a2183baf8b2669303fe2f90426ce18477e8ead
Gabriel Siqueira <gabriel.siqueira@globant.com>
2024-11-12 12:03:21 -0300

    fix: correcoes

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 let countdown;
+let isRunning = false; 
 
 function startTimer() {
     const timeInput = document.getElementById('timeInput').value;
     let time = timeInput * 60; 
     clearInterval(countdown);
+
     if(time <= 0){
         document.getElementById('showMessage').style.display = 'block';
         document.getElementById('showMessage').innerText = 'Digite um valor para funcionar.';
         setTimeout(()=> {
             document.getElementById('showMessage').style.display = 'none';
-        },3000)
+        }, 3000);
         return;
     }
+
+    isRunning = true; 
     countdown = setInterval(() => {
+        if (time <= 0) {
+            clearInterval(countdown);
+            isRunning = false; 
+            return;
+        }
+
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
         document.getElementById('timeInput').style.display = 'none';
@@ -21,4 +31,9 @@ function startTimer() {
             `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
         time--;
     }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(countdown);
+    isRunning = false; 
 }
